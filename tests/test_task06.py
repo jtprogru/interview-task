762ba3d94b750b8b2daf96bb3a8aa945bc4a5de8
Michael Savin <m.savin@corp.mail.ru>
2021-11-02 11:42:00 +0300

    Add Task06

---
+++ tests/test_task06.py
@@ -0,0 +1,16 @@
+import pytest
+
+from tasks.task06 import solution
+
+
+@pytest.mark.parametrize(
+    "digits, result",
+    [
+        ([1, 2, 3], [1, 2, 4]),
+        ([4, 3, 2, 1], [4, 3, 2, 2]),
+        ([0], [1]),
+        ([9], [1, 0]),
+    ],
+)
+def test_task06_solution(digits, result):
+    assert solution(digits) == result
