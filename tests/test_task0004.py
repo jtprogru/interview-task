012e7824d1195e62ac5e35abe8332c5adab92098
Michael Savin <m.savin@corp.mail.ru>
2021-11-03 21:33:02 +0300

    Обновил тесты для всех задачек

--- tests/test_task0004.py
+++ tests/test_task0004.py
@@ -9,6 +9,10 @@ from tasks.task0004 import solution
         ([1, 2, 3, 1], True),
         ([1, 2, 3, 4], False),
         ([1, 3, 3, 4, 3, 2, 4, 2], True),
+        ([0], False),
+        ([0, 1], False),
+        ([0, 1, 2], False),
+        ([0, 1, 1], True),
     ],
 )
 def test_task04_solution(nums, result):
--- tests/test_task0006.py
+++ tests/test_task0006.py
@@ -9,7 +9,9 @@ from tasks.task0006 import solution
         ([1, 2, 3], [1, 2, 4]),
         ([4, 3, 2, 1], [4, 3, 2, 2]),
         ([0], [1]),
+        ([1], [2]),
         ([9], [1, 0]),
+        ([3, 0, 9], [3, 1, 0]),
     ],
 )
 def test_task06_solution(digits, result):
