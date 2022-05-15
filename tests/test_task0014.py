6286457ddda2471bea08b220198c1b1a8738298d
Michael Savin <jtprogru@gmail.com>
2022-05-15 22:18:16 +0300

    Fix linters

--- tests/test_task0014.py
+++ tests/test_task0014.py
@@ -5,12 +5,7 @@ from tasks.task0014 import solution
 
 @pytest.mark.parametrize(
     "nums, result",
-    [
-        ([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
-        ([0], [0]),
-        ([0, 1], [1, 0]),
-        ([1, 0], [1, 0])
-    ],
+    [([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]), ([0], [0]), ([0, 1], [1, 0]), ([1, 0], [1, 0])],
 )
 def test_task0014_solution(nums, result):
     solution(nums)
