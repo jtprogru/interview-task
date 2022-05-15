473c42dc6b4832f7a379824d3816afaef255cd1c
Michael Savin <jtprogru@gmail.com>
2022-05-15 21:54:59 +0300

    Add task0014
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

---
+++ tests/test_task0014.py
@@ -0,0 +1,17 @@
+import pytest
+
+from tasks.task0014 import solution
+
+
+@pytest.mark.parametrize(
+    "nums, result",
+    [
+        ([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
+        ([0], [0]),
+        ([0, 1], [1, 0]),
+        ([1, 0], [1, 0])
+    ],
+)
+def test_task0014_solution(nums, result):
+    solution(nums)
+    assert nums == result
