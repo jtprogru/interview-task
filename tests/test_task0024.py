ffe2e040daaca3d1076cdc0b00fc67c28913f006
Michael Savin <jtprogru@gmail.com>
2022-06-05 20:58:05 +0300

    Fix tests/test_task0024.py
    Signed-off-by: Michael Savin <jtprogru@gmail.com>

--- tests/test_task0024.py
+++ tests/test_task0024.py
@@ -5,12 +5,7 @@ from tasks.task0024 import solution
 
 @pytest.mark.parametrize(
     "haystack, needle, result",
-    [
-        ("hello", "ll", 2),
-        ("aaaaa", "bba", -1),
-        ("aaaaa", "", 0),
-        ("a", "a", 0)
-    ],
+    [("hello", "ll", 2), ("aaaaa", "bba", -1), ("aaaaa", "", 0), ("a", "a", 0)],
 )
 def test_task0024_solution(haystack, needle, result):
     assert solution(haystack, needle) == result
